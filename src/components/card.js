dbc415684ba99efef40a8c8800f251f5163057f3
desiree <desireegarcia@mini.hsd1.nm.comcast.net>
2021-10-01 15:33:54 -0600

    completed task 6

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { response } from "msw";
 
 const Card = (article) => {
   // TASK 5
@@ -39,7 +40,7 @@ const Card = (article) => {
   author.appendChild(imgContainer);
   imgContainer.appendChild(authorPhoto);
   author.appendChild(authorName);
-console.log(card);
+// console.log(card);
 
   //creating classes
   card.classList.add('card');
@@ -55,21 +56,42 @@ console.log(card);
 }
 
 const cardAppender = (selector) => {
-  // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `http://localhost:5000/api/articles` (test it in Postman/HTTPie!).
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
-  // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
+//   // TASK 6
+//   // ---------------------
+//   // Implement this function that takes a css selector as its only argument.
+//   // It should obtain articles from this endpoint: `http://localhost:5000/api/articles` (test it in Postman/HTTPie!).
+//   // However, the articles do not come organized in a single, neat array. Inspect the response closely!
+//   // Create a card from each and every article object in the response, using the Card component.
+//   // Append each card to the element in the DOM that matches the selector passed to the function.
+//   //
   axios.get('http://localhost:5000/api/articles')
-  .then(resp => {
-    // console.log(resp);
-    // console.log(resp.data);
-   //lets create an array of the data
-    const newData = Object.entries(resp.data.articles);
-    console.log(newData);
+  .then(resp => {    
+    // console.log(resp.data.articles);
+
+    const cardItems = document.querySelector(selector);
+    //javascript
+    resp.data.articles.javascript.forEach(item => {
+    cardItems.appendChild(Card(item));
+    });
+    //bootstrap
+    resp.data.articles.bootstrap.forEach(item => {
+      cardItems.appendChild(Card(item));
+    });
+    //technology
+    resp.data.articles.technology.forEach(item => {
+      cardItems.appendChild(Card(item));
+    });
+    //jquery
+    resp.data.articles.jquery.forEach(item => {
+      cardItems.appendChild(Card(item));
+    });
+    //node.js
+    resp.data.articles.node.forEach(item => {
+      cardItems.appendChild(Card(item));
+    })
+  })
+  .catch(err => {
+    console.error(err)
   })
 }
 
